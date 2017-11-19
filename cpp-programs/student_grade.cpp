1c775af56eaf9b0e72511dd02f19e2eb25a18413
Kevin Leimkuhler <me@kleimkuhler.com>
2017-11-19 14:16:54 -0800

    Change homework grade to work with median

--- cpp-programs/student_grade.cpp
+++ cpp-programs/student_grade.cpp
@@ -1,11 +1,14 @@
+#include <algorithm>
 #include <iomanip>
 #include <ios>
 #include <iostream>
 #include <string>
+#include <vector>
 
 using std::cin;         using std::setprecision;
 using std::cout;        using std::string;
 using std::endl;        using std::streamsize;
+using std::vector;      using std::sort;
 
 int main()
 {
@@ -22,27 +25,42 @@ int main()
 
     // ask for the homework grades
     cout << "Enter all your homework grades, "
-        "follow by end-of-file: ";
+            "follow by end-of-file: ";
 
     // the number and sum of grades read so far
     int count = 0;
     double sum = 0;
 
-    // a variable into which to read
-    double x;
+    // a vector into which to read
+    vector<double> homework;
 
-    // invariant:
-    // read count grades so far, and sum is the sum of the
-    // first count grades
-    while (cin >> x) {
-        ++count;
-        sum += x;
+    // invariant: homework contains all the homework grades so far
+    double x;
+    while (cin >> x)
+        homework.push_back(x);
+
+    typedef vector<double>::size_type vec_sz;
+    vec_sz size = homework.size();
+
+    // meaningless to find median of empty dataset
+    if (size == 0) {
+        cout << "You must enter your grades. "
+                "Please try again." << endl;
+        return 1;
     }
 
+    // sort the grades
+    sort(homework.begin(), homework.end());
+
+    // compute median homework grade
+    vec_sz mid = size/2;
+    double median;
+    median = size % 2 == 0? (homework[mid] + homework[mid+1]) / 2
+                          : homework[mid];
     // write the result
     streamsize prec = cout.precision();
     cout << "Your final grade is " << setprecision(3)
-        << 0.2 * midterm + 0.4 * final + 0.4 * sum / count
+        << 0.2 * midterm + 0.4 * final + 0.4 * median
         << setprecision(prec) << endl;
 
     return 0;
