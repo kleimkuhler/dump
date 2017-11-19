d4741ac8efbc93ebc3f5444f079199cf4e12aa4a
Kevin Leimkuhler <me@kleimkuhler.com>
2017-11-18 21:13:47 -0800

    Expand message frame

--- cpp-programs/message_frame.cpp
+++ cpp-programs/message_frame.cpp
@@ -1,30 +1,63 @@
 #include <iostream>
 #include <string>
 
+// say what stand-library names to use
+using std::cin;         using std::endl;
+using std::cout;        using std::string;
+
 int main()
 {
-  // ask for the person's name
-  std::cout << "Please enter your first name: ";
-  std::string name;  // define name
-  std::cin >> name;  // read into
+    // define inputs
+    string name;
+    int v_pad;
+    int h_pad;
 
-  // build the message that we intend to write
-  const std::string greeting = "Hello, " + name + "!";
+    // ask for inputs
+    cout << "Please enter your first name: ";
+    cin >> name;
+    cout << "Vertical padding: ";
+    cin >> v_pad;
+    cout << "Horiztonal padding: ";
+    cin >> h_pad;
 
-  // build the second and fourth lines of the output
-  const std::string spaces(greeting.size(), ' ');
-  const std::string second = "* " + spaces + " *";
+    // build the message
+    const string greeting = "Hello, " + name + "!";
 
-  // build the first and fifth lines of the output;
-  const std::string first(second.size(), '*');
+   // total number of rows and columns to write
+    const int rows = v_pad * 2 + 3;
+    const string::size_type cols = greeting.size() + h_pad * 2 + 2;
+    const string spaces = string(cols - 2, ' ');
 
-  // write it all
-  std::cout << std::endl;
-  std::cout << first << std::endl;
-  std::cout << second << std::endl;
-  std::cout << "* " << greeting << " *" << std::endl;
-  std::cout << second << std::endl;
-  std::cout << first << std::endl;
+    // separate the output from the input
+    std::cout << std::endl;
 
-  return 0;
+    // write rows of output
+    for (int r = 0; r != rows; ++r) {
+        string::size_type c = 0;
+
+        // invariant: written c characters so far in the current row
+        while (c != cols) {
+            if (r == 0 || r == rows - 1 ||
+               c == 0 || c == cols - 1) {
+                cout << "*";
+                ++c;
+            }
+            else if (r == v_pad + 1) {
+                if (c == h_pad + 1) {
+                    cout << greeting;
+                    c += greeting.size();
+                } else {
+                    cout << " ";
+                    ++c;
+                }
+            } else {
+                cout << spaces;
+                c += spaces.size();
+            } 
+        }
+
+        cout << endl;
+    }
+
+    return 0;
 }
