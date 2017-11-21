5540de48e7c508bb13fe164d02eca1e119248d28
Kevin Leimkuhler <me@kleimkuhler.com>
2017-11-20 21:39:27 -0800

    Separate some calculations into functions

--- cpp-programs/student_grade.cpp
+++ cpp-programs/student_grade.cpp
@@ -2,13 +2,36 @@
 #include <iomanip>
 #include <ios>
 #include <iostream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
-using std::cin;         using std::setprecision;
-using std::cout;        using std::string;
-using std::endl;        using std::streamsize;
-using std::vector;      using std::sort;
+using std::cin;          using std::setprecision;
+using std::cout;         using std::string;
+using std::endl;         using std::streamsize;
+using std::vector;       using std::sort;
+using std::domain_error;
+
+typedef vector<double>::size_type vec_sz;
+
+double grade(double midterm, double final, double homework)
+{
+    return 0.2 * midterm + 0.4 * final + 0.4 * homework;
+}
+
+double median(vector<double> vec)
+{
+    vec_sz size = vec.size();
+    if (size == 0) {
+        throw domain_error("median of an empty vector");
+    }
+
+    sort(vec.begin(), vec.end());
+
+    vec_sz mid = size / 2;
+
+    return size % 2 == 0 ? (vec[mid] + vec[mid-1]) / 2 : vec[mid];
+}
 
 int main()
 {
@@ -39,29 +62,11 @@ int main()
     while (cin >> x)
         homework.push_back(x);
 
-    typedef vector<double>::size_type vec_sz;
-    vec_sz size = homework.size();
-
-    // meaningless to find median of empty dataset
-    if (size == 0) {
-        cout << "You must enter your grades. "
-                "Please try again." << endl;
-        return 1;
-    }
-
-    // sort the grades
-    sort(homework.begin(), homework.end());
-
-    // compute median homework grade
-    vec_sz mid = size/2;
-    double median;
-    median = size % 2 == 0? (homework[mid] + homework[mid+1]) / 2
-                          : homework[mid];
     // write the result
     streamsize prec = cout.precision();
     cout << "Your final grade is " << setprecision(3)
-        << 0.2 * midterm + 0.4 * final + 0.4 * median
-        << setprecision(prec) << endl;
+	 << grade(midterm, final, median(homework))
+         << setprecision(prec) << endl;
 
     return 0;
 }
