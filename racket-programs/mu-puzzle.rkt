b5e79ad90f39c29a3b15293bbc057bfba72d7d90
Kevin Leimkuhler <me@kleimkuhler.com>
2018-01-27 15:26:07 -0800

    Make a general `replace` procedure
    Refactored `xIIIy->xUy` and `xUUy->xy` with a general `replace`
    procedure so that there was less duplication.

--- racket-programs/mu-puzzle.rkt
+++ racket-programs/mu-puzzle.rkt
@@ -8,53 +8,50 @@
 ;; This puzzle caught my attention as a good way to think about this formal
 ;; system outside of the book and incorporate it into a program.
 
+(define (match start result substr count)
+  ;; Match the `count` occurence of `substr` in `start` and return two lists
+  ;; that equal ``start` - `count` occurence of `substr``
+  (cond ((< (length start) (length substr)) (cons '() '()))
+        ((equal? substr (take start (length substr)))
+         (if (= (- count 1) 0)
+             (let ((remain (list-tail start (length substr))))
+               (cons result remain))
+             (match (cdr start) (cons (car start) result) substr (- count 1))))
+        (else
+         (match (cdr start) (cons (car start) result) substr count))))
+
 (define (MU-puzzle)
   ;; MU puzzle with axiomatic string MI
   (let ((str '(M I)))
+    (define (replace old new count)
+      ;; Replace the `count` occurence of `old` with `new` in `str`
+      (if (= count 0)
+          "Occurence must be greater than 0"
+          (let ((matched (match str '() old count)))
+            (let ((result (car matched))
+                  (remain (cdr matched)))
+              (if (null? result)
+                  "Occurence not found"
+                  (begin (set! str (append (reverse result) new remain))
+                         str))))))
     (define (xI->xIU)
-      ;; If `str` ends in I then append a U to `str`
       (if (eq? 'I (last str))
           (begin (set! str (reverse (cons 'U (reverse str))))
                  str)
           "Puzzle must end in I"))
     (define (Mx->Mxx)
-      ;; Replicate the string after M and append it to `str`
       (let ((x (cdr str)))
         (set! str (append str x))
         str))
-    (define (xIIIy->xUy start result)
-      ;; Find the `count` occurrence of III and replace it with U
-      (lambda (count)
-        (cond ((and (= count 0) (null? result)) "Occurence must be greater than 0")
-              ((< (length start) 3) "Occurrence not found")
-              ((equal? '(I I I) (list (car start) (cadr start) (caddr start)))
-               (if (= (- count 1) 0)
-                   (let ((remain (cdddr start)))
-                     (begin (set! str (append (reverse result) '(U) remain))
-                            str))
-                   ((xIIIy->xUy (cdr start) (cons (car start) result)) (- count 1))))
-              (else
-               ((xIIIy->xUy (cdr start) (cons (car start) result)) count)))))
-    (define (xUUy->xy start result)
-      ;; Find the `count` occurrence of UU and replace it with ''
-      (lambda (count)
-        (cond ((and (= count 0) (null? result)) "Occurence must be greater than 0")
-              ((< (length start) 2) "Occurrence not found")
-              ((equal? '(U U) (list (car start) (cadr start)))
-               (if (= (- count 1) 0)
-                   (let ((remain (cddr start)))
-                     (begin (set! str (append (reverse result) remain))
-                            str))
-                   ((xUUy->xy (cdr start) (cons (car start) result)) (- count 1))))
-              (else
-               ((xUUy->xy (cdr start) (cons (car start) result)) count)))))
+    (define (xIIIy->xUy count) (replace '(I I I) '(U) count))
+    (define (xUUy->xy count) (replace '(U U) '() count))
     (define (print) str)
     (define (dispatch f)
       ;; Dispatch transformation procedures on `str`
       (cond ((eq? f 'xI->xIU) xI->xIU)
             ((eq? f 'Mx->Mxx) Mx->Mxx)
-            ((eq? f 'xIIIy->xUy) (xIIIy->xUy str '()))
-            ((eq? f 'xUUy->xy) (xUUy->xy str '()))
+            ((eq? f 'xIIIy->xUy) xIIIy->xUy)
+            ((eq? f 'xUUy->xy) xUUy->xy)
             ((eq? f 'print) print)
             (else (error "Unkown request: MU-PUZZLE"
                          f))))
